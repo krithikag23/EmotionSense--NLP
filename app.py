5a083dfc009220076ee9d70801bd62ad082051de
Krithika Ganesan <113279833+krithikag23@users.noreply.github.com>
2025-11-10 19:21:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ model_name = "j-hartmann/emotion-english-distilroberta-base"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
+
 label_names = ['anger','disgust','fear','joy','neutral','sadness','surprise']
+
 
 st.title("🎭 EmotionSense AI")
 st.write("Enter text and I will detect the emotion!")
 
+
 user_input = st.text_input("Type something:")
+
 
 if user_input:
     inputs = tokenizer(user_input, return_tensors="pt")
