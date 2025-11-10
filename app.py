522f2b9c3a217d108a183098cd43df140ee58a98
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-11-10 19:15:32 +0530

    update

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+
+model_name = "j-hartmann/emotion-english-distilroberta-base"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+label_names = ['anger','disgust','fear','joy','neutral','sadness','surprise']
+
+st.title("🎭 EmotionSense AI")
+st.write("Enter text and I will detect the emotion!")
+
+user_input = st.text_input("Type something:")
+
+if user_input:
+    inputs = tokenizer(user_input, return_tensors="pt")
+    logits = model(**inputs).logits
+    pred = torch.argmax(logits).item()
+    emotion = label_names[pred]
+    st.success(f"**Emotion:** {emotion}")
